091116c77480e824dc320e7694e0e3b53bf02746
chaitanybhute <161194386+chaitanybhute@users.noreply.github.com>
2025-09-22 07:20:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -107,7 +107,12 @@ app.use("/listing", listingRouter);
 app.use("/listing/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+// other route definitions above...
 
+// Catch-all for unhandled routes (should be last)
+app.get('*', (req, res) => {
+  res.render('index.ejs'); // or your main EJS template
+});
 
 //Error handling Middlewere
 
